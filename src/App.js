c6ee774c77b28659d562b5c123bc7dd1997f4cb8
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-02-13 22:53:28 +0530

    error page using route

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 import Store from './Components/Routes/Store';
 import About from "./Components/Routes/About"
 import Home from "./Components/Routes/Home"
+import ErrorPage from './Components/Routes/ErrorPage';
+
 
 const router=createBrowserRouter([
-  {path:"/",element:<Store/>},
+  {path:"/",element:<Store/>,errorElement:<ErrorPage/>},
   {path:"/about",element:<About/>},
   {path:"/home",element:<Home/>},
   {path:"/store", element:<Store/>}
---
+++ src/Components/Routes/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./ErrorPage.css"
+const ErrorPage = () => {
+  return (
+    <div className='body'>
+      <h1 style={{color:"red",textAlign:'center',marginTop:"20%"}} > error !</h1>
+      <p style={{textAlign:"center"}}>looks like your encountered an error</p>
+    </div>
+  )
+}
+
+export default ErrorPage
